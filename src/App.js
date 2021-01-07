45e71c24bee0168d66fb823dac14f75d9a9ebbd1
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-01-07 21:47:21 +0800

    使用 前端路由

--- src/App.js
+++ src/App.js
@@ -1,48 +1,33 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import { Container } from "@material-ui/core";
-import Header from "./components/Header";
+import { useRoutes } from "react-router-dom";
+import UserMenu from "./components/UserMenu";
+import NotFound from "./components/Util/NotFound";
 import Dashboard from "./components/Dashboard";
+
+import Home from "./components/Home";
+
 import Questionaires from "./components/Questionaire/Questionaires";
-import Contents from "./components/Content/Contents";
+
+import ContentStage from "./components/Content/components/ContentStage";
+import ContentLecture from "./components/Content/components/ContentLecture";
+import ContentVideo from "./components/Content/components/ContentVideo";
 
 function App() {
+  let element = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/content/stages", element: <ContentStage /> },
+    { path: "/content/stage/:stage_id", element: <ContentLecture /> },
+    { path: "/content/video/:video_id", element: <ContentVideo /> },
+    { path: "/questionnaire", element: <Questionaires /> },
+    { path: "/dashboard", element: <Dashboard /> },
+    // Not found routes work as you'd expect
+    { path: "*", element: <NotFound /> },
+  ]);
+
   return (
     <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/">
-          <Contents />
-        </Route>
-        <Route path="/exercises">
-          {/* <Exercises /> */}
-          <h1>習題系統</h1>
-        </Route>
-        <Route path="/misssions">
-          <h1>任務系統</h1>
-          {/* <Missions /> */}
-        </Route>
-        <Route path="/classroom">
-          <h1>班級系統</h1>
-        </Route>
-        <Route path="/account">
-          <h1>帳號系統</h1>
-        </Route>
-        <Route path="/questionnaire">
-          <Questionaires />
-        </Route>
-        <Route path="/visualization">
-          <h1>資料視覺化系統</h1>
-        </Route>
-        <Route path="/special-contents">
-          <h1>主題式課程系統</h1>
-        </Route>
-        <Route exact path="/dashboard">
-          <Container maxWidth="xl">
-            <Dashboard />
-          </Container>
-        </Route>
-      </Switch>
+      <UserMenu />
+      {element}
     </div>
   );
 }
