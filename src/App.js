14771bde3eec46a2b9981f6c44ec1885601d29fc
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-01-11 09:06:19 +0800

    add menu

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import UserMenu from "./components/UserMenu";
 import NotFound from "./components/Util/NotFound";
 import Dashboard from "./components/Dashboard";
 
+// 首頁
 import Home from "./components/Home";
-
+// 問卷系統
 import Questionaires from "./components/Questionaire/Questionaires";
-
+// 任務系統
+import Missions from "./components/Missions/Missions";
+// 習題系統
+import Exercises from "./components/Exercises/Exercises";
+// 班級系統
+import Classrooms from "./components/Classrooms/Classrooms";
+// 課程系統
 import ContentStage from "./components/Content/components/ContentStage";
 import ContentLecture from "./components/Content/components/ContentLecture";
 import ContentVideo from "./components/Content/components/ContentVideo";
@@ -19,6 +26,9 @@ function App() {
     { path: "/content/stage/:stage_id", element: <ContentLecture /> },
     { path: "/content/video/:video_id", element: <ContentVideo /> },
     { path: "/questionnaire", element: <Questionaires /> },
+    { path: "/misssions", element: <Missions /> },
+    { path: "/exercises", element: <Exercises /> },
+    { path: "/classroom", element: <Classrooms /> },
     { path: "/dashboard", element: <Dashboard /> },
     // Not found routes work as you'd expect
     { path: "*", element: <NotFound /> },
---
+++ src/components/UserMenu.jsx
@@ -0,0 +1,232 @@
+import React, { useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import MailIcon from "@material-ui/icons/Mail";
+import FaceIcon from "@material-ui/icons/Face";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import CachedIcon from "@material-ui/icons/Cached";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  },
+  titleWrapper: {
+    flexGrow: 1,
+    textDecoration: "none",
+  },
+  title: {
+    fontFamily: "Work Sans, sans-serif",
+    fontWeight: 600,
+    color: "#FFFEFE",
+    textAlign: "left",
+    fontSize: 30,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-start",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  },
+}));
+
+export default function UserMenu() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleOnClick = (herf) => {
+    window.location.href = herf;
+  };
+
+  const fakeData = [
+    {
+      title: "課程影片",
+      href: "/content/stages",
+    },
+    {
+      title: "習題系統",
+      href: "/exercises",
+    },
+    {
+      title: "任務系統",
+      href: "/misssions",
+    },
+    {
+      title: "班級系統",
+      href: "/classroom",
+    },
+    {
+      title: "問卷系統",
+      href: "/questionnaire",
+    },
+    {
+      title: "資料視覺化系統",
+      href: "/visualization",
+    },
+    {
+      title: "Dashboard",
+      href: "/dashboard",
+    },
+  ];
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <ClickAwayListener onClickAway={handleDrawerClose}>
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <RouterLink to="/" className={classes.titleWrapper}>
+              <Typography variant="h6" component="h1" className={classes.title}>
+                APP
+              </Typography>
+            </RouterLink>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="end"
+              onClick={handleDrawerOpen}
+              className={clsx(open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </ClickAwayListener>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
+
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="right"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          {
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          }
+        </div>
+        <Divider />
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <FaceIcon />
+            </ListItemIcon>
+            <ListItemText primary={"Kai"} />
+          </ListItem>
+          {fakeData.map((headersData, index) => (
+            <ListItem
+              button
+              key={headersData.title}
+              onClick={() => handleOnClick(headersData.href)}
+            >
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={headersData.title} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["身分切換", "登出"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <CachedIcon /> : <ExitToAppIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
+}
