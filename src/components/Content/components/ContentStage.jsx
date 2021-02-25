2ad1c5fa522bf1d64bdd3149048c5b9af4fe5086
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-02-25 16:21:38 +0800

    整理 ContentStage UI

--- src/components/Content/components/ContentStage.jsx
+++ src/components/Content/components/ContentStage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import {
@@ -14,6 +14,7 @@ import {
 const useStyles = makeStyles({
   media: {
     height: 140,
+    margin: 20,
     backgroundSize: "contain",
   },
 });
@@ -27,18 +28,18 @@ const EntryButton = (props) => {
   };
 
   return (
-    <Grid item xs={12} md={props.type ? 6 : 4}>
+    <Grid item xs={12} md={4}>
       <Card>
         <CardActionArea
-          onClick={() => handleNavigate("/content/stage/" + props.stage)}
+          onClick={() => handleNavigate("/content/stage/" + props.stageData.id)}
         >
           <CardMedia
             className={classes.media}
-            image={props.type ? `/ThemeEntryLogo.svg` : `/BasicEntryLogo.svg`}
+            image={props.stageData.icon_src}
           />
           <CardContent>
-            <Typography variant="h4" align="center">
-              {props.title}
+            <Typography variant="h4" align="center" noWrap={true}>
+              {props.stageData.title}
             </Typography>
           </CardContent>
         </CardActionArea>
@@ -47,43 +48,69 @@ const EntryButton = (props) => {
   );
 };
 
-const SyllabusEntry = () => {
-  return (
-    <Grid container spacing={3} justify="center">
-      <Grid item xs={12}>
-        <Box p={5}>
-          <Typography variant="h3" align="center">
-            <b>108課綱 教材</b>
-          </Typography>
-        </Box>
-      </Grid>
-      <EntryButton title="國小" stage={0} type={0} />
-      <EntryButton title="國中" stage={1} type={0} />
-      <EntryButton title="高中" stage={2} type={0} />
-    </Grid>
-  );
-};
-
-const ThemeEntry = () => {
-  return (
-    <Grid container spacing={3} justify="center">
-      <Grid item xs={12}>
-        <Box p={5} mt={3}>
-          <Typography variant="h3" align="center">
-            <b>主題式 教材</b>
-          </Typography>
-        </Box>
-      </Grid>
-      <EntryButton title="主題式" stage={99} type={1} />
-    </Grid>
-  );
-};
-
 export default function ContentStage() {
+  const classes = useStyles();
+  const [stageData, setStageData] = useState([]);
+
+  useEffect(() => {
+    // 取得 Stage 資料
+    const FAKE_STAGE_DATA = [
+      {
+        id: 0,
+        title: "國小",
+        icon_src: "/BasicEntryLogo.svg",
+      },
+      {
+        id: 1,
+        title: "國中",
+        icon_src: "/BasicEntryLogo.svg",
+      },
+      {
+        id: 2,
+        title: "高中",
+        icon_src: "/BasicEntryLogo.svg",
+      },
+    ];
+    setStageData(FAKE_STAGE_DATA);
+  }, []);
+
   return (
     <Box p={6}>
-      {SyllabusEntry()}
-      {ThemeEntry()}
+      {/* 課綱 */}
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12}>
+          <Box p={5}>
+            <Typography variant="h3" align="center">
+              <b>108課綱 教材</b>
+            </Typography>
+          </Box>
+        </Grid>
+        {stageData.map((stage) => {
+          return <EntryButton stageData={stage} key={stage.id} />;
+        })}
+      </Grid>
+      {/* 主題式 */}
+      <Grid container spacing={3} justify="center">
+        <Grid item xs={12}>
+          <Box p={5} mt={3}>
+            <Typography variant="h3" align="center">
+              <b>主題式 教材</b>
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+      <Grid item xs={12}>
+        <Card>
+          <CardActionArea>
+            <CardMedia className={classes.media} image="/ThemeEntryLogo.svg" />
+            <CardContent>
+              <Typography variant="h4" align="center" noWrap={true}>
+                主題式
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
     </Box>
   );
 }
