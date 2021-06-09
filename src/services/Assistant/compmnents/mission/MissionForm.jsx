7d16a98b6f983a43481e9e39dffc06eb0a036fb0
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-06-09 22:28:50 +0800

    新增任務 UI

---
+++ src/services/Assistant/compmnents/mission/MissionForm.jsx
@@ -0,0 +1,155 @@
+import React, { useEffect } from "react";
+import {
+  Grid,
+  FormControl,
+  InputLabel,
+  Select,
+  FormHelperText,
+  makeStyles,
+} from "@material-ui/core";
+import Controls from "../../../Utility/compmnents/Controls/Controls";
+import { useForm, Form } from "../../../Utility/compmnents/UseForm";
+
+const useStyles = makeStyles((theme) => ({
+  buttonWrapper: {
+    display: "flex",
+    justifyContent: "center",
+    margin: theme.spacing(2),
+  },
+}));
+
+const isAllowAddItems = [
+  { id: "Video", value: "1", title: "影片任務" },
+  { id: "Exercise", value: "2", title: "習題任務" },
+];
+
+const initialFValues = {
+  id: String(new Date().getTime()),
+  type: "1",
+  name: "",
+  unit: "",
+  exercise: "",
+};
+
+export default function MissionForm({
+  units,
+  exercises,
+  missionForEdit = null,
+  handleAddOrEdit,
+}) {
+  const validate = (fieldValues = values) => {
+    let temp = { ...errors };
+    if ("name" in fieldValues)
+      temp.name = fieldValues.name ? "" : "任務名稱為必填";
+    if ("unit" in fieldValues && fieldValues.type === "1")
+      temp.unit = fieldValues.unit ? "" : "影片為必填";
+    if ("exercise" in fieldValues && fieldValues.type === "2")
+      temp.exercise = fieldValues.exercise ? "" : "習題為必填";
+
+    setErrors({
+      ...temp,
+    });
+
+    if (fieldValues === values)
+      return Object.values(temp).every((x) => x == "");
+  };
+
+  const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
+    useForm(initialFValues, true, validate);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      handleAddOrEdit(values, resetForm);
+    }
+  };
+
+  useEffect(() => {
+    if (missionForEdit != null)
+      setValues({
+        ...missionForEdit,
+      });
+  }, [missionForEdit]);
+
+  const classes = useStyles();
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Grid container>
+        <Grid item xs={12}>
+          <Controls.RadioGroup
+            name="type"
+            label="種類"
+            value={values.type}
+            onChange={handleInputChange}
+            items={isAllowAddItems}
+            required
+          />
+          <Controls.Input
+            label="任務名稱"
+            name="name"
+            value={values.name}
+            onChange={handleInputChange}
+            error={errors.name}
+          />
+          {values.type === "1" && (
+            <FormControl variant="outlined" fullWidth error={errors.unit}>
+              <InputLabel htmlFor="exercise">影片</InputLabel>
+              <Select
+                native
+                value={values.unit}
+                onChange={handleInputChange}
+                inputProps={{
+                  name: "unit",
+                  id: "unit",
+                }}
+                label="影片"
+              >
+                <option aria-label="None" value="" />
+                {units.map((unit) => {
+                  return (
+                    <option key={unit.id} value={unit.id}>
+                      {unit.title}
+                    </option>
+                  );
+                })}
+              </Select>
+              <FormHelperText>影片為必填</FormHelperText>
+            </FormControl>
+          )}
+          {values.type === "2" && (
+            <FormControl variant="outlined" fullWidth error={errors.exercise}>
+              <InputLabel htmlFor="exercise">習題</InputLabel>
+              <Select
+                native
+                value={values.exercise}
+                onChange={handleInputChange}
+                inputProps={{
+                  name: "exercise",
+                  id: "exercise",
+                }}
+                label="標籤"
+              >
+                <option aria-label="None" value="" />
+                {exercises.map((exercise) => {
+                  return (
+                    <option key={exercise.id} value={exercise.id}>
+                      {exercise.title}
+                    </option>
+                  );
+                })}
+              </Select>
+              <FormHelperText>習題為必填</FormHelperText>
+            </FormControl>
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          <div className={classes.buttonWrapper}>
+            <Controls.Button type="submit" text="送出" />
+            <Controls.Button text="清除" color="default" onClick={resetForm} />
+          </div>
+        </Grid>
+      </Grid>
+    </Form>
+  );
+}
--- src/services/Assistant/pages/Missions.jsx
+++ src/services/Assistant/pages/Missions.jsx
@@ -1,15 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Paper, makeStyles } from "@material-ui/core";
+import Swal from "sweetalert2";
+import axios from "axios";
+import { Paper, Grid, Button, makeStyles } from "@material-ui/core";
 import FlagIcon from "@material-ui/icons/Flag";
 
 import PageHeader from "../../Utility/compmnents/PageHeader";
+import OperatorMenu from "../../Utility/compmnents/OperatorMenu";
+import MissionForm from "../compmnents/mission/MissionForm";
+import Popup from "../../Utility/compmnents/Popup";
 
 const useStyles = makeStyles((theme) => ({
   pageContent: {
     margin: theme.spacing(5),
     padding: theme.spacing(3),
+  },
+  menuButton: {
+    marginRight: 16,
   },
 }));
 
@@ -18,11 +26,89 @@ export default function Missions() {
   const navigate = useNavigate();
   const userState = useSelector((state) => state.userState);
 
+  const [exercises, setExercises] = useState([]);
+  const [units, setUnits] = useState([]);
+
   useEffect(() => {
     if (!userState.user && !userState.isChecking) {
       navigate("/auth/login");
     }
+
+    if (userState.user) {
+      // 取得考試卷清單
+      axios({
+        method: "GET",
+        url: `${process.env.REACT_APP_CONTENT_SERVICE}/exercises/`,
+        headers: {
+          token: `${localStorage.jwt}`,
+          user: `${userState.user._id}`,
+        },
+        params: {
+          owner: userState.user._id,
+        },
+      }).then((result) => {
+        console.log("考試卷:", result.data);
+        setExercises(result.data);
+      });
+
+      // 取得影片清單
+      axios({
+        method: "GET",
+        url: `${process.env.REACT_APP_CONTENT_SERVICE}/units`,
+        headers: {
+          token: `${localStorage.jwt}`,
+          user: `${userState.user._id}`,
+        },
+        withCredentials: true,
+      })
+        .then((result) => {
+          console.log("影片:", result.data);
+          setUnits(result.data);
+        })
+        .catch((err) => {
+          console.error(err);
+          Swal.fire({
+            icon: "error",
+            title: "新增班級失敗!",
+          });
+        });
+    }
   }, [userState]);
+
+  const [openPopup, setOpenPopup] = useState(false);
+
+  const handleAdd = (newMission, resetForm) => {
+    resetForm();
+    setOpenPopup(false);
+
+    console.log("新任務:", newMission);
+
+    // axios({
+    //   method: "POST",
+    //   url: `${process.env.REACT_APP_CONTENT_SERVICE}/missions`,
+    //   headers: {
+    //     token: `${localStorage.jwt}`,
+    //     user: `${userState.user._id}`,
+    //   },
+    //   data: newMission,
+    //   withCredentials: true,
+    // })
+    //   .then((result) => {
+    //     console.log(result.data)
+    //     Swal.fire({
+    //       icon: "success",
+    //       title: `新增任務 ${result.data.name} 成功!`,
+    //       width: 700,
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //     Swal.fire({
+    //       icon: "error",
+    //       title: "新增任務失敗!",
+    //     });
+    //   });
+  };
 
   const classes = useStyles();
 
@@ -33,7 +119,38 @@ export default function Missions() {
         subTitle="subTitle"
         icon={<FlagIcon fontSize="large" />}
       />
-      {!userState.isChecking && <Paper className={classes.pageContent}></Paper>}
+      {(userState?.user?.role === "Admin" ||
+        userState?.user?.role === "Teacher" ||
+        userState?.user?.role === "Parent") && (
+        <OperatorMenu>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.menuButton}
+            onClick={() => {
+              setOpenPopup(true);
+            }}
+          >
+            新增任務
+          </Button>
+        </OperatorMenu>
+      )}
+      {!userState.isChecking && (
+        <Paper className={classes.pageContent}>
+          <Grid container spacing={3}>
+            <Grid item md={12}></Grid>
+            <Grid item md={12}></Grid>
+            <Grid item md={12}></Grid>
+          </Grid>
+        </Paper>
+      )}
+      <Popup title="新增任務" openPopup={openPopup} setOpenPopup={setOpenPopup}>
+        <MissionForm
+          units={units}
+          exercises={exercises}
+          handleAddOrEdit={handleAdd}
+        />
+      </Popup>
     </>
   );
 }
