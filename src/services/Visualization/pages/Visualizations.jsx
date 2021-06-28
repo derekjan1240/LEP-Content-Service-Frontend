1092b184db82859199aaa7a953b6d13e97a7b552
Kai-Jian Zhan (Derek) <derekjan1240@gmail.com>
2021-06-28 21:33:07 +0800

    資料視覺化 新增選單

--- src/services/Visualization/pages/Visualizations.jsx
+++ src/services/Visualization/pages/Visualizations.jsx
@@ -1,15 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Paper, makeStyles } from "@material-ui/core";
+import Swal from "sweetalert2";
+import axios from "axios";
+import moment from "moment";
+import {
+  Paper,
+  Grid,
+  Box,
+  Button,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
 import BarChartIcon from "@material-ui/icons/BarChart";
 
 import PageHeader from "../../Utility/compmnents/PageHeader";
+
+import teal from "@material-ui/core/colors/teal";
+
+const infoTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: teal[600],
+      contrastText: "#fff",
+    },
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
   pageContent: {
     margin: theme.spacing(5),
     padding: theme.spacing(3),
+  },
+  menuButton: {
+    marginRight: 16,
+  },
+  questionnaireCard: {
+    borderBottom: "1px dashed #636e72",
   },
 }));
 
@@ -18,9 +47,35 @@ export default function Visualizations() {
   const navigate = useNavigate();
   const userState = useSelector((state) => state.userState);
 
+  const [questionnaireResults, setQuestionnaireResults] = useState([]);
+  const [display, setDisplay] = useState("1");
+
   useEffect(() => {
     if (!userState.user && !userState.isChecking) {
       navigate("/auth/login");
+    }
+    if (userState.user) {
+      axios({
+        method: "GET",
+        url: `${process.env.REACT_APP_QUESTIONNAIRE_SERVICE}/questionnaires/result`,
+        headers: {
+          token: `${localStorage.jwt}`,
+          user: `${userState.user._id}`,
+        },
+      })
+        .then((result) => {
+          console.log("學生問卷清單:", result.data);
+          setQuestionnaireResults(
+            result.data.filter((result) => result.is_complated === true)
+          );
+        })
+        .catch((err) => {
+          console.error(err);
+          Swal.fire({
+            icon: "error",
+            title: "查詢已指派問卷清單失敗!",
+          });
+        });
     }
   }, [userState]);
 
@@ -33,7 +88,102 @@ export default function Visualizations() {
         subTitle="圖像化系統數據"
         icon={<BarChartIcon fontSize="large" />}
       />
-      {!userState.isChecking && <Paper className={classes.pageContent}></Paper>}
+      {!userState.isChecking && (
+        <Paper className={classes.pageContent}>
+          <Grid container spacing={3}>
+            <Grid item md={12}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.menuButton}
+                onClick={() => {
+                  setDisplay("1");
+                }}
+              >
+                問卷分析報告
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.menuButton}
+                onClick={() => {
+                  setDisplay("2");
+                }}
+              >
+                學生分析報告
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.menuButton}
+                onClick={() => {
+                  setDisplay("3");
+                }}
+              >
+                考卷分析報告
+              </Button>
+            </Grid>
+            {display === "1" &&
+              questionnaireResults.map((questionnaireResult) => {
+                return (
+                  <Grid item md={12} key={questionnaireResult._id}>
+                    <Box p={3} className={classes.questionnaireCard}>
+                      <Typography variant="h4" gutterBottom>
+                        <b>{questionnaireResult.questionnaire.name}</b>
+                      </Typography>
+                      <Typography variant="h6" gutterBottom>
+                        所屬班級: {questionnaireResult.classroom.name}
+                      </Typography>
+                      <Typography variant="h6" gutterBottom>
+                        指派對象: {questionnaireResult.assignee?.userName}
+                      </Typography>
+                      <Typography variant="h6" gutterBottom>
+                        完成時間:{" "}
+                        {moment(questionnaireResult.complated_date).format(
+                          "YYYY/MM/DD HH:mm"
+                        )}
+                      </Typography>
+                      <Box display="flex" alignItems="center" mt={3}>
+                        <ThemeProvider theme={infoTheme}>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.menuButton}
+                            onClick={() => {
+                              navigate(
+                                `/visualization/questionnaire/${questionnaireResult._id}`
+                              );
+                            }}
+                          >
+                            查看分析報告
+                          </Button>
+                        </ThemeProvider>
+                      </Box>
+                    </Box>
+                  </Grid>
+                );
+              })}
+            {display === "2" && (
+              <Grid item md={12}>
+                <Box p={3} className={classes.questionnaireCard}>
+                  <Typography variant="h4" gutterBottom>
+                    學生分析報告
+                  </Typography>
+                </Box>
+              </Grid>
+            )}
+            {display === "3" && (
+              <Grid item md={12}>
+                <Box p={3} className={classes.questionnaireCard}>
+                  <Typography variant="h4" gutterBottom>
+                    試卷分析報告
+                  </Typography>
+                </Box>
+              </Grid>
+            )}
+          </Grid>
+        </Paper>
+      )}
     </>
   );
 }
